a24ca8ab0b19c6d2eb84afc8d3bbc8fb21b7f04b
syphax <syphaxmez@gmail.com>
2023-11-11 21:25:34 -0500

    Ajoutez des fichiers projet.

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
